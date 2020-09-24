79a0c7f718b48fd0ee2929b999507980c33cae16
Michael Mottola <mottola64@gmail.com>
2020-09-24 00:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function inOrder(currentNode){
     inOrder(currentNode.right)
   }
 }
+
+function findOrAdd(currentNode, newNode){
+  if(currentNode.data == newNode.data){
+    return true
+  }
+  if(newNode.data < currentNode.data){
+    if(currentNode.left){
+      return findOrAdd(currentNode.left, newNode)
+    } else {
+      return currentNode.left = newNode
+    }
+  }
+
+  if(newNode.data > currentNode.data){
+    if(currentNode.right){
+      return findOrAdd(currentNode.right, newNode)
+    } else {
+      return currentNode.right = newNode
+    }
+  }
+}
